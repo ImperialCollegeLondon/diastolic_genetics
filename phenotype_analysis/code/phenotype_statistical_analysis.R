867847ed0201a72d807e2757704810d1ff326d03
Marjola Thanaj <55489483+Marjola89@users.noreply.github.com>
2020-10-05 10:50:28 +0100

    update

--- phenotype_analysis/code/phenotype_statistical_analysis.R
+++ phenotype_analysis/code/phenotype_statistical_analysis.R
@@ -14,7 +14,6 @@ conflist<-vector(mode="list",length=30)
 
 iT<-1
 for (iN in 12:41){
-  data_na<-na.omit(as.data.frame(multidata[,c(1:11,iN)]))
   cv<-lapply(colnames(data_na)[12], function(x) lm(formula(paste("`",x,"`","~.", sep="")),data=data_na))
   pval<-summary(cv[[1]])$coefficients[,"Pr(>|t|)"] # get p-values
   p.cor<-p.adjust(pval,method = "BH") # adjust using Benjamini - Hochberg procedure
@@ -58,9 +57,10 @@ pos_pheno<-match(rownames(pheno_all),colnames(pheno)) # order the position of va
 
 
 library(data.table)
-multivariate_data <- read.table("multivar_datatable.txt", header = TRUE) # load the whole dataset
-multivariate_data_train <- read.table("multivar_train_datatable.txt", header = TRUE) # load the training dataset
-colnames(multivariate_data_train)<-colnames(multivariate_data)
+multivar_data <- read.table("multivar_datatable.txt", header = TRUE) # load the whole dataset
+multivar_data_train <- read.table("multivar_train_datatable.txt", header = TRUE) # load the training dataset
+multivar_data_test <- read.table("multivar_test_datatable.txt", header = TRUE) # load the test dataset
+colnames(multivar_data_train)<-colnames(multivar_data)
 covar <- read.table("cov.txt", header = TRUE) # the position of all non-imaging covariates
 pos_pheno <- read.table("fit.txt", header = TRUE) # the position of imaging covariates
 ## load data for training and for analysis
@@ -71,7 +71,7 @@ position_stab<-rbind(covar,pos_pheno)
 
 install.packages("mctest")
 
-model<-lm(`PDSRll (s-1)`~., data=as.data.frame(multivariate_data[,position_stab[,1]]))
+model<-lm(`PDSRll (s-1)`~., data=as.data.frame(multivar_data[,position_stab[,1]]))
 library(mctest)
 imcdiag(model,method="VIF", vif=5) # 0 if collinearity is not detected by this test
 
@@ -80,7 +80,7 @@ imcdiag(model,method="VIF", vif=5) # 0 if collinearity is not detected by this t
 library(glmnet)  
 
 # cv.glmnet to train for the lambda parameter
-data.train<-as.matrix(multivariate_data_train[,position_stab[,1]])
+data.train<-as.matrix(multivar_data_train[,position_stab[,1]])
 data.train<-na.omit(data.train)
 colnames(data.train)
 lambda_min<-matrix(0,ncol = 1, nrow = ncol(data.train))
@@ -94,7 +94,7 @@ for (iT in 1:ncol(data.train)){
 }
 
 # glmnet 
-data_selected<-as.matrix(multivariate_data[,position_stab[,1]])
+data_selected<-as.matrix(multivar_data_test[,position_stab[,1]])
 data_selected<-na.omit(data_selected)
 beta_gl<-matrix(0,ncol = ncol(data_selected), nrow = ncol(data_selected)-1)
 for (iS in 1:ncol(data_selected)){
@@ -114,8 +114,8 @@ for (iS in 1:ncol(data_selected)){
 
 colnames(beta_gl)<-colnames(data_selected)
 beta_gl<-as.data.frame(beta_gl)
-multivariate_beta<-matrix(0,nrow = ncol(beta_gl),ncol=ncol(beta_gl))
-multivariate_beta<-as.data.frame(multivariate_beta)
-write.table(multivariate_beta, "multivariate_beta.txt", row.names = T, col.names = T) # save beta coefficients
+multivar_beta<-matrix(0,nrow = ncol(beta_gl),ncol=ncol(beta_gl))
+multivar_beta<-as.data.frame(multivar_beta)
+write.table(multivar_beta, "multivar_beta.txt", row.names = T, col.names = T) # save beta coefficients
 
 # END
