99148c569f16aee5e6c0bd697988e1fde14ad271
Marjola Thanaj <55489483+Marjola89@users.noreply.github.com>
2021-04-07 13:15:10 +0100

    update

--- phenotype_analysis/code/phenotype_imputation/data_imputation.R
+++ phenotype_analysis/code/phenotype_imputation/data_imputation.R
@@ -19,6 +19,3 @@ meth[no]<-"" # do not impute race
 mice_impute=mice(data_pheno,m=5,maxit=5, method = meth, print = T)
 impute_data=complete(mice_impute,5) # get the last imputation
 data_imp<-as.data.frame(impute_data) # imputed data
-
-# Save 
-# write.table(data_imp,"Phenotypes_40k_imp.txt", col.names = T)
